7e024b0e88723d84deb7d491f4a93e0a70bea519
AlexandraHockett <alexandramandio@gmail.com>
2024-11-27 13:15:20 +0000

    Implement Hero section with dynamic features, reusable MagicButton, and styling - Added Hero section with a welcoming heading and subheading. - Integrated dynamic text effect using TextGenerateEffect for engaging intro. - Included FlipWords component to display rotating tech stack (ReactJS, Next.js, etc.). - Created and used a reusable MagicButton component for navigating to other sections, with customizable icon and title. - Applied responsive design with Tailwind CSS to ensure optimization across all screen sizes. - Enhanced visual appeal using multiple Spotlight elements for a modern background. - Added radial gradient mask effect for the background with dark mode support. - Structured Hero section to serve as the focal point of the portfolio, with clean typography and clear call-to-action.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,43 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import React from "react";
+import React, { ReactNode } from "react";
+import { ThemeProvider } from "@/components/Provider";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
 export const metadata: Metadata = {
   title: "Alexandra - Front-End Developer Portfolio",
   description:
     "Front-end developer with hospitality roots, specializing in React and Next.js. Transforming career through tech skills and passionate about creating innovative web solutions.",
 };
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+
+const RootLayout = async ({ children }: { children: ReactNode }) => {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
-}
+};
+export default RootLayout;
