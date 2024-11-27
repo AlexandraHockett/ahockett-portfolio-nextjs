2de10c45a8186c5e2a9d44bc810626aeb9ee4156
AlexandraHockett <alexandramandio@gmail.com>
2024-11-27 12:43:15 +0000

    Project Setup

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import React from "react";
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+export const metadata: Metadata = {
+  title: "Alexandra - Front-End Developer Portfolio",
+  description:
+    "Front-end developer with hospitality roots, specializing in React and Next.js. Transforming career through tech skills and passionate about creating innovative web solutions.",
+};
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
+      <div className="w-full max-w-7xl">
+        <h1>Hello Portfolio</h1>
+      </div>
+    </main>
+  );
+}
